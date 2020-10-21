a12e4314548be39754b2354c8a4ffa939fed0617
Jan Küchle <jancolach@gmail.com>
2020-10-21 19:05:57 +0200

    auf .NET Framework geändert

---
+++ UnitTestProject1/CoolesSpiel2/Calc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolesSpiel
+{
+    public class Calc
+    {
+        public int Distance(int x, int y)
+        {
+            if(x >= y)
+            {
+                return x - y;
+            }
+            else
+            {
+                return y - x;
+            }
+        }
+    }
+}
