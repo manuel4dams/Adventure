b384cb59a32738f0786d0a077e21ec8648f26cd4
manueladams <manuel.adams@hs-weingarten.com>
2020-11-20 16:10:51 +0100

    program class not needed

--- Game/ForestAdventure/GameWindow.cs
+++ Game/ForestAdventure/GameWindow.cs
@@ -1,4 +1,5 @@
-﻿using OpenTK;
+﻿using System.Reflection;
+using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Input;
 
@@ -9,6 +10,20 @@ namespace ForestAdventure
         public GameWindow(int width, int height, string title)
             : base(width, height, GraphicsMode.Default, title)
         {
+        }
+
+        public static void Main()
+        {
+            GameWindow gameWindow = new GameWindow(1000, 1000, Assembly.GetExecutingAssembly().GetName().Name);
+
+            gameWindow.WindowState = WindowState.Maximized;
+            // gameWindow.UpdateFrame += (objectArgs, args) => model.Update((float) args.Time);
+            // gameWindow.Resize += (objectArgs, args) => view.Resize(window.Width, window.Height);
+            // gameWindow.RenderFrame += (objectArgs, frameEventArgs) => view.Draw(model);
+            // gameWindow.RenderFrame += (objectArgs, frameEventArgs) => window.SwapBuffers();
+
+            // start the game loop with 60Hz
+            gameWindow.Run(60);
         }
 
         protected override void OnUpdateFrame(FrameEventArgs e)
