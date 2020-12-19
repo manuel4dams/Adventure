733d83ae89c23094d5b097d0d85cc42b8cd32613
adamsmanuel <adams5464@gmail.com>
2020-12-19 19:14:11 +0100

    generate stub methods

--- Game/Framework/Render/TextRenderer.cs
+++ Game/Framework/Render/TextRenderer.cs
@@ -1,7 +1,28 @@
-﻿namespace Framework.Render
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+using System.Net.Mime;
+using Framework.Game;
+using Framework.Interfaces;
+using OpenTK.Graphics.OpenGL;
+using PixelFormat = System.Drawing.Imaging.PixelFormat;
+
+namespace Framework.Render
 {
-    public class TextRenderer
+    public class TextRenderer : IComponent, IRender
     {
         // TODO maybe needed for rendering texts
+
+        public TextRenderer()
+        {
+        }
+
+        public GameObject gameObject { get; }
+
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
