0df0d1be5a89c5a2099cd09f5673157a9a5a192f
adamsmanuel <adams5464@gmail.com>
2021-01-13 09:59:47 +0100

    wip

--- Game/ForestAdventure/Develop/LevelMover.cs
+++ Game/ForestAdventure/Develop/LevelMover.cs
@@ -20,10 +20,10 @@ namespace ForestAdventure.Develop
         public void Update(float deltaTime)
         {
             var keyboardState = Keyboard.GetState();
-            var left = keyboardState.IsKeyDown(Key.Left) || keyboardState.IsKeyDown(Key.A) ? -1f : 0f;
-            var right = keyboardState.IsKeyDown(Key.Right) || keyboardState.IsKeyDown(Key.D) ? 1f : 0f;
-            var up = keyboardState.IsKeyDown(Key.Up) || keyboardState.IsKeyDown(Key.W) ? 1f : 0f;
-            var down = keyboardState.IsKeyDown(Key.Down) || keyboardState.IsKeyDown(Key.S) ? -1f : 0f;
+            var left = keyboardState.IsKeyDown(Key.Left) || keyboardState.IsKeyDown(Key.A) ? -4f : 0f;
+            var right = keyboardState.IsKeyDown(Key.Right) || keyboardState.IsKeyDown(Key.D) ? 4f : 0f;
+            var up = keyboardState.IsKeyDown(Key.Up) || keyboardState.IsKeyDown(Key.W) ? 4f : 0f;
+            var down = keyboardState.IsKeyDown(Key.Down) || keyboardState.IsKeyDown(Key.S) ? -4f : 0f;
 
             transform.position += 10f * deltaTime * new Vector2(
                 left + right,
--- Game/ForestAdventure/ForestAdventure.cs
+++ Game/ForestAdventure/ForestAdventure.cs
@@ -35,64 +35,50 @@ namespace ForestAdventure
 
         private static void InitLevel()
         {
-            Game.instance.AddGameObject(new ForestBackground());
+            // Game.instance.AddGameObject(new ForestBackground());
             AddPlatforms();
             AddRopes();
             AddCheckpoints();
             Game.instance.AddGameObject(new Exit(new Vector2(60f, 97.8f)));
             AddEnemies();
-            // Game.instance.AddGameObject(new Player(new Vector2(0f, 0f)));
-            Game.instance.AddGameObject(new LevelMover());
+            Game.instance.AddGameObject(new Player(new Vector2(0f, 0f)));
+            // Game.instance.AddGameObject(new LevelMover());
             Game.instance.AddGameObject(camera);
             Game.instance.AddGameObject(new BottomLevelBorder(new Vector2(0f, -20f), 500));
         }
 
         private static void AddPlatforms()
         {
-            // floor 1
             Game.instance.AddGameObject(new Platform(new Vector2(0f, -1.5f), 6f));
             Game.instance.AddGameObject(new Platform(new Vector2(12f, 2f), 6f));
             Game.instance.AddGameObject(new Platform(new Vector2(24f, 3f), 6f));
             Game.instance.AddGameObject(new Platform(new Vector2(36f, 4f), 6f));
             Game.instance.AddGameObject(new Platform(new Vector2(48f, 6f), 6f));
-            // floor 2
             Game.instance.AddGameObject(new Platform(new Vector2(61f, 12f), 6f));
-            // floor 3
             Game.instance.AddGameObject(new Platform(new Vector2(24f, 18f), 14f));
             Game.instance.AddGameObject(new Platform(new Vector2(48f, 18f), 6f));
-            // floor 4
             Game.instance.AddGameObject(new Platform(new Vector2(3f, 24f), 6f));
             Game.instance.AddGameObject(new Platform(new Vector2(61f, 24f), 6f));
-            // floor 5
             Game.instance.AddGameObject(new Platform(new Vector2(16f, 30f), 6f));
-            // floor 6
             Game.instance.AddGameObject(new Platform(new Vector2(3f, 36f), 6f));
-            // floor 7
             Game.instance.AddGameObject(new Platform(new Vector2(16f, 42f), 6f));
             Game.instance.AddGameObject(new Platform(new Vector2(40f, 42f), 14f));
-            // floor 8
             Game.instance.AddGameObject(new Platform(new Vector2(61f, 48f), 6f));
-            // floor 9
             Game.instance.AddGameObject(new Platform(new Vector2(48f, 54f), 6f));
-            // floor 10
             Game.instance.AddGameObject(new Platform(new Vector2(61f, 60f), 6f));
             Game.instance.AddGameObject(new Platform(new Vector2(24f, 60f), 14f));
             Game.instance.AddGameObject(new Platform(new Vector2(40f, 60f), 6f));
-            // floor 11a
             Game.instance.AddGameObject(new Platform(new Vector2(16f, 66f), 6f));
-            // floor 12
             Game.instance.AddGameObject(new Platform(new Vector2(3f, 72f), 6f));
-            // floor 13
             Game.instance.AddGameObject(new Platform(new Vector2(16f, 78f), 6f));
             Game.instance.AddGameObject(new Platform(new Vector2(48f, 78f), 14f));
             Game.instance.AddGameObject(new Platform(new Vector2(29f, 82f), 6f));
-            // floor 14
             Game.instance.AddGameObject(new Platform(new Vector2(3f, 84f), 6f));
             Game.instance.AddGameObject(new Platform(new Vector2(61f, 84f), 6f));
-            // floor 15
             Game.instance.AddGameObject(new Platform(new Vector2(48f, 90f), 6f));
-            // floor 16
             Game.instance.AddGameObject(new Platform(new Vector2(61f, 96f), 6f));
+            Game.instance.AddGameObject(new Platform(new Vector2(72f, 74f), 6f));
+            Game.instance.AddGameObject(new Platform(new Vector2(81f, 80f), 14f));
         }
 
         private static void AddRopes()
@@ -102,19 +88,19 @@ namespace ForestAdventure
 
         private static void AddCheckpoints()
         {
+            // y = platformY + 1.5f
             Game.instance.AddGameObject(new Checkpoint(new Vector2(0f, 0f)));
         }
 
         private static void AddEnemies()
         {
-            // floor 3
+            // values for 14f long Platform
+            // enemyY = platformY +1.8f, movementBorderLeft = x - 6, movementBorderRight = x + 6
             Game.instance.AddGameObject(new Enemy(new Vector2(24f, 19.8f), 18f, 30f));
-            // floor 7
             Game.instance.AddGameObject(new Enemy(new Vector2(40f, 43.8f), 34f, 46f));
-            // floor 10
             Game.instance.AddGameObject(new Enemy(new Vector2(24f, 61.8f), 18f, 30f));
-            // floor 13
             Game.instance.AddGameObject(new Enemy(new Vector2(48f, 79.8f), 42f, 54f));
+            Game.instance.AddGameObject(new Enemy(new Vector2(81f, 81.8f), 75f, 87f));
         }
     }
 }
